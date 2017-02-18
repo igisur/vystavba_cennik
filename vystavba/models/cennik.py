1ca22274bf57e07f9869101d8bc7b1f0f6d30418
branislav.vilmon <branislav.vilmon@gmail.com>
2017-02-19 00:13:47 +0100

    CP polozky oddelene mj jednotkova cena celkova cena s menou detail polozky CP aj popis polozky

--- vystavba/models/cennik.py
+++ vystavba/models/cennik.py
@@ -22,10 +22,12 @@ class VystavbaCennikPolozka(models.Model):
     _name = 'vystavba.cennik.polozka'
     _description = "Vystavbovy cennik - cena polozky cennika pre konkretneho partnera"
 
-    name = fields.Char(related='polozka_id.name', string='Merná jednotka')
+    #name = fields.Char(related='polozka_id.name', string='Názov')
+    kod = fields.Char(related='polozka_id.kod', string='Kód')
+    popis = fields.Text(related='polozka_id.description', string='Popis')
     cena = fields.Float(required=True, digits=(10, 2))
-    cennik_id = fields.Many2one('vystavba.cennik', string='Odkaz na cennik', required=True, ondelete='cascade')
-    polozka_id = fields.Many2one('vystavba.polozka', string='Polozka', required=True)
+    cennik_id = fields.Many2one('vystavba.cennik', string='Cenník', required=True, ondelete='cascade')
+    polozka_id = fields.Many2one('vystavba.polozka', string='Položka', required=True)
     polozka_mj = fields.Selection(related='polozka_id.mj', string='Merná jednotka')
 
     @api.multi
