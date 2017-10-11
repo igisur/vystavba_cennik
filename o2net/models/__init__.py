0338cce3409f4c6c58f229559e589e5903b65ab8
igisur <igor.surovy@gmail.com>
2017-10-11 22:11:54 +0200

    prepare folder structure for multi semi-module solution

--- o2net/models/__init__.py
+++ o2net/models/__init__.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import partner
-import product
-import pricelist
-import pricelist_item
-import section
-import quotation
-import quotation_item
-import quotation_item_atyp
-import quotation_item_package
 import res_config
-import wizards
+import partner
+import quot
---
+++ o2net/models/quot/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import product
+import pricelist
+import pricelist_item
+import section
+import quotation
+import quotation_item
+import quotation_item_atyp
+import quotation_item_package
+import wizards
